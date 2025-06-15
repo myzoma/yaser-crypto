0b3914f05824e481bf9a4f7f27a72bcc1c417caa
myzoma <86922707+myzoma@users.noreply.github.com>
2025-06-15 15:12:38 +0300

    Create hunter-data.js

---
+++ yaser-crypto/hunter-data.js
@@ -0,0 +1,16 @@
+window.hunterOpportunities = [
+  {
+    "symbol": "SOLUSDT",
+    "price": 156.32,
+    "change24h": 3.5,
+    "opportunityType": "اختراق مقاومة",
+    "opportunityStrength": "🔥"
+  },
+  {
+    "symbol": "AVAXUSDT",
+    "price": 27.9,
+    "change24h": 2.4,
+    "opportunityType": "زخم شرائي",
+    "opportunityStrength": "🚀"
+  }
+];

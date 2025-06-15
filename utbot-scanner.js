08e6cc408f089cde7633d4a7cb24a21d7c285d6c
myzoma <86922707+myzoma@users.noreply.github.com>
2025-06-15 20:36:15 +0300

    Update utbot-scanner.js

--- utbot-scanner.js
+++ utbot-scanner.js
@@ -10,7 +10,7 @@ class UTBotScanner {
             // ضع مفاتيح OKX هنا
             apiKey: 'b20c667d-ae40-48a6-93f4-a11a64185068',
             secretKey: 'BD7C76F71D1A4E01B4C7E1A23B620365',
-            passphrase: '212160Nm$#',
+            passphrase: '212160Nm$#'
         }
     };
         this.symbols = [];
